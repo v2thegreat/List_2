f987bac8dff357a6285b03ff78550a88f861cbe2
v2thegreat <aliabbaszaidi@outlook.com>
2017-12-17 19:07:22 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='list_2',
+      version='0.1.1',
+      description='Wrapper on Python's Numpy Array to make it work like a Python list for memory efficiency',
+      url='http://github.com/storborg/funniest',
+      author='Ali Abbas',
+      author_email='aliabbaszaidi@outlook.com',
+      license='GNU AFFERO GENERAL PUBLIC LICENSE',
+      packages=['list_2'],
+      install_requires=['numpy'],
+      zip_safe=False)
